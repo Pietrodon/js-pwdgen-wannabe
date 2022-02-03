24075a6b0da341c4e74aaa941e1c68dbb8926175
Pietrodon <donvitopietro00@gmail.com>
2022-02-03 17:28:28 +0100

    modifche html e inserimento prompt per il colore preferito

--- app.js
+++ app.js
@@ -4,3 +4,6 @@ console.log(userName)
 
 let userSurname = prompt('quale è il tuo cognome?')
 console.log(userSurname)
+
+let userColor = prompt('quale è il tuo colore preferito?')
+console.log(userColor)
